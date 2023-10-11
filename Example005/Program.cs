e5016b2b0ae33b163bb2456fd8dc16ddeb881b74
yamaksimk <mkurakin2001@gmail.com>
2023-10-11 17:23:41 +0300

    example 005

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 % n2 == 0)
+    Console.WriteLine("кратно");
+else
+        Console.WriteLine("Остаток от деления = " + n1 % n2);
