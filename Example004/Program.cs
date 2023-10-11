9288bbcb5c3ce2ed92affd87dbf34fdb5e0eaa5b
yamaksimk <mkurakin2001@gmail.com>
2023-10-11 16:55:09 +0300

    example 004

---
+++ Example004/Program.cs
@@ -0,0 +1,15 @@
+﻿// int a = 5;
+// int b = 10;
+
+// if (a == 5 && b == 10)
+//     Console.WriteLine("Yes");
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 7 == 0 && n % 23 == 0)
+    Console.WriteLine("Yes");
+else
+        Console.WriteLine("No");
