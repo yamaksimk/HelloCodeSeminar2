2f732887d2f1e17ad79f754af504701e301d2f5a
yamaksimk <mkurakin2001@gmail.com>
2023-10-11 15:59:39 +0300

    example 002

---
+++ Example002/Program.cs
@@ -0,0 +1,6 @@
+﻿string name = "Maksim";
+int age = 22;
+string city = "Moscow";
+
+Console.WriteLine("Меня зовут " + name + " и мне " + age + " года. Я из " + city);
+Console.WriteLine($"Меня зовут {name} и мне {age} года. Я из {city}");
