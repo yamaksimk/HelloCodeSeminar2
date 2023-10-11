37dcee0b6ca7021dcd654734d13b86ead012f315
yamaksimk <mkurakin2001@gmail.com>
2023-10-11 16:39:45 +0300

    example 003

---
+++ Example003/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+int n = new Random().Next(100, 1000);
+Console.WriteLine(n);
+
+int n1 = n / 100;
+int n3 = n % 10;
+
+Console.WriteLine($"{n1}{n3}");
+Console.WriteLine(n1 * 10 + n3);
