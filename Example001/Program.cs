c8b233e443852482ee72ccebaf3b59c6222329fc
yamaksimk <mkurakin2001@gmail.com>
2023-10-11 15:11:09 +0300

    example001

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,4 +1,12 @@
 ﻿Console.Clear();
 
 int n = new Random().Next(10, 100);
-Console.WriteLine(n);
+Console.WriteLine("Рандомное число" + n);
+
+int n1 = n / 10;
+int n2 = n % 10;
+
+if (n1 > n2) 
+    Console.WriteLine(n1);
+else
+    Console.WriteLine(n2);
